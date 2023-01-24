08829c249d0ff0f3fe0e7ae2d29db8c999107ea8
Andre Smith <andre.frank.smith@gmail.com>
2023-01-24 09:08:52 +0100

    Simple change to comment

--- client/src/app/shared/utils/pipes/size-unit.pipe.ts
+++ client/src/app/shared/utils/pipes/size-unit.pipe.ts
@@ -3,7 +3,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({name: 'unit', standalone: true})
 export class SizeUnitPipe implements PipeTransform {
   transform(value: any): any {
-    // kg for now, but this could be a user preference setting later.
+    // kb for now, but this could be a user preference setting later.
     return `${value / 1000} KB`;
 
   }
